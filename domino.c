6c88d0c5b20bf5f784ca7630e76e53c997c45e62
Kauan Novello <50461490+kauan-novello@users.noreply.github.com>
2023-11-20 13:42:34 -0300

    Update domino.c

--- domino.c
+++ domino.c
@@ -17,10 +17,11 @@ struct Dorme {
     struct Pecas pecas[4];
 }dorme;
 
+void jogadaPLayer(int player, struct Pecas pecas[]);
 
 int main() {
     
-    srand(time(NULL)); // Inicializa 
+    srand(time(NULL)); 
 
     struct Pecas pecas[] = {
         {1, 0,0},     {2, 0,1},     {3, 0,2},     {4, 0,3},     {5, 0,4},     {6, 0,5},    {7, 0,6},
@@ -44,59 +45,8 @@ int main() {
         pecas[i] = pecas[j];
         pecas[j] = temp;
     }
-
-    // Imprime as peças de dominó embaralhadas
-    // for (int i = 0; i < tamanho; i++) {
-    //     printf("ID: %d, Nome: %s\n", pecas[i].id, pecas[i].left, pecas[i].rigth);
-    // }
-
-    //Distribuir peça dos jogadores e do dorme
-
-    printf("Pecas do jogador 1 (p1):\n\n");
-    for (int i = 0; i < 6; i++) {
-        p1.pecas[i] = pecas[i];
-        printf("ID: %02d, Peca: [%d|%d]\n", p1.pecas[i].id, p1.pecas[i].left, p1.pecas[i].rigth);
-    }
-    printf("\n");
-
-    printf("Pecas do jogador 2 (p2):\n\n");
-    for (int i = 6; i < 12; i++) {
-        p2.pecas[i] = pecas[i];
-        printf("ID: %02d, Peca: [%d|%d]\n", p2.pecas[i].id, p2.pecas[i].left, p2.pecas[i].rigth);
-    }
-    printf("\n");
     
-    printf("Pecas do jogador 3 (p3):\n\n");
-    for (int i = 12; i < 18; i++) {
-        p3.pecas[i] = pecas[i];
-        printf("ID: %02d, Peca: [%d|%d]\n", p3.pecas[i].id, p3.pecas[i].left, p3.pecas[i].rigth);
-    }
-    printf("\n");
-
-    printf("Pecas do jogador 4 (p4):\n\n");
-    for (int i = 18; i < 24; i++) {
-        p4.pecas[i] = pecas[i];
-        printf("ID: %02d, Peca: [%d|%d]\n", p4.pecas[i].id, p4.pecas[i].left, p4.pecas[i].rigth);
-    }
-    printf("\n");
-    
-    printf("Pecas do Dorme:\n\n");
-    for (int i = 24; i < 28; i++) {
-        dorme.pecas[i] = pecas[i];
-        printf("ID: %02d, Peca: [%d|%d]\n", dorme.pecas[i].id, dorme.pecas[i].left, dorme.pecas[i].rigth);
-    }
-    printf("\n\n");
-
-     int rodada = 1;
-
-    printf("Pressione alguma tecla para comecar");
-    char tecla;
-    getchar(); 
-
-    while (rodada--)
-    {
-
-        system("cls");
+    void mostrarPecas(){
         printf("Pecas do jogador 1 (p1):\n\n");
         for (int i = 0; i < 6; i++) {
             p1.pecas[i] = pecas[i];
@@ -104,18 +54,80 @@ int main() {
         }
         printf("\n");
 
-        printf("Faca sua jogada");
-        getchar(); 
-
-        //transformar jogadas em função
+        printf("Pecas do jogador 2 (p2):\n\n");
+        for (int i = 6; i < 12; i++) {
+            p2.pecas[i - 6] = pecas[i];
+            printf("ID: %02d, Peca: [%d|%d]\n", p2.pecas[i - 6].id, p2.pecas[i - 6].left, p2.pecas[i - 6].rigth);
+        }
+        printf("\n");
         
-        break;
+        printf("Pecas do jogador 3 (p3):\n\n");
+        for (int i = 12; i < 18; i++) {
+            p3.pecas[i - 12] = pecas[i]; 
+            printf("ID: %02d, Peca: [%d|%d]\n", p3.pecas[i - 12].id, p3.pecas[i - 12].left, p3.pecas[i - 12].rigth);
+        }
+        printf("\n");
+
+        printf("Pecas do jogador 4 (p4):\n\n");
+        for (int i = 18; i < 24; i++) {
+            p4.pecas[i - 18] = pecas[i]; 
+            printf("ID: %02d, Peca: [%d|%d]\n", p4.pecas[i - 18].id, p4.pecas[i - 18].left, p4.pecas[i - 18].rigth);
+        }
+        printf("\n");
+        
+        printf("Pecas do Dorme:\n\n");
+        for (int i = 24; i < 28; i++) {
+            dorme.pecas[i - 24] = pecas[i]; 
+            printf("ID: %02d, Peca: [%d|%d]\n", dorme.pecas[i - 24].id, dorme.pecas[i - 24].left, dorme.pecas[i - 24].rigth);
+        }
+        printf("\n\n");
+    }
+    mostrarPecas();
+
+    int rodada = 8;
+
+    printf("Pressione alguma tecla para comecar  ");
+    getchar(); 
+
+    while (rodada > 0) {
+        int playerdavez = rodada % 4;
+        jogadaPLayer(playerdavez, pecas);
+        rodada--;
     }
     
     // pra começar o jogo na rodada 1 o player que tiver o 6/6 começa, caso esteja no dorme, vai para o 5/5 e assim sucessivamente...
+
     // faça a opção de jogada e remova a peça quando jogada
     
     
 
     return 0;
+}
+
+void jogadaPLayer(int player, struct Pecas pecas[]) {
+    //int player = player;
+    system("cls"); //system("clear");
+    printf("Vez do jogador %d:\n", player+1);
+
+    printf("Suas pecas: \n");
+    for (int i = 0; i < 6; i++) {
+        
+        if (player == 0) {
+            printf("ID: %02d, Peca: [%d|%d]\n", p1.pecas[i].id, p1.pecas[i].left, p1.pecas[i].rigth);
+        }
+        if (player == 1) {
+            printf("ID: %02d, Peca: [%d|%d]\n", p2.pecas[i].id, p2.pecas[i].left, p2.pecas[i].rigth);
+        }
+        if (player == 2) {
+            printf("ID: %02d, Peca: [%d|%d]\n", p3.pecas[i].id, p3.pecas[i].left, p3.pecas[i].rigth);
+        }
+        if (player == 3) {
+            printf("ID: %02d, Peca: [%d|%d]\n", p4.pecas[i].id, p4.pecas[i].left, p4.pecas[i].rigth);
+        }
+
+    }
+            
+    printf("\nEscolha o Id da peca que deseja jogar: ");
+
+    getchar();
 }
